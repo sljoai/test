84290897b0e6c0829571c7066e948d8e777a4fc6
songlaijiang <99026795@qq.com>
2018-12-13 23:25:02 +0800

    添加Kafka Consumer seek 指定主题和分区、偏移量

--- test4kafka/src/main/java/com/song/cn/kafka/comsumer/Main.java
+++ test4kafka/src/main/java/com/song/cn/kafka/comsumer/Main.java
@@ -3,6 +3,7 @@ package com.song.cn.kafka.comsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +30,13 @@ public class Main {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Arrays.asList("test")); // subscribe "test" topic
+        //consumer.subscribe(Arrays.asList("test")); // subscribe "test" topic
+        //分配topic和partition
+        //consumer.assign(Arrays.asList(new TopicPartition("test",0)));
+        //指定从这个topic和partition的开始位置获取
+        consumer.seekToBeginning(Arrays.asList(new TopicPartition("test", 0)));//不改变当前offset
+        //从指定topic的分区及偏移量开始读取消息
+        consumer.seek(new TopicPartition("test",0),10);//不改变当前offset
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
--- test4kafka/src/main/java/com/song/cn/kafka/producer/Main.java
+++ test4kafka/src/main/java/com/song/cn/kafka/producer/Main.java
@@ -3,11 +3,14 @@ package com.song.cn.kafka.producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class Main {
 
@@ -19,8 +22,8 @@ public class Main {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.80.235:9092,192.168.80.235:9093,192.168.80.235:9094"); // broker list
-        props.put("acks", "all");
+        props.put("bootstrap.servers", "192.168.80.235:9093"); // broker list
+        props.put("acks", "1");
         props.put("retries", 0);
         props.put("batch.size", 16384);
         props.put("linger.ms", 1);
@@ -34,7 +37,14 @@ public class Main {
         for(int i = 0; i < 10; i++) {
             System.out.println(i);
             // send record to topic 'test'
-            producer.send(new ProducerRecord<String, String>("test", "key"+ Integer.toString(i), "value" + Integer.toString(i)));
+            Future<RecordMetadata> metadata = producer.send(new ProducerRecord<String, String>("test", "key"+ Integer.toString(i), "value" + Integer.toString(i)));
+            try {
+                metadata.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
 
         producer.close();
