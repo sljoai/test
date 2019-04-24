1c73d00ff373d84a991a4b11640a0c6a41b9c3d0
songlaijiang <99026795@qq.com>
2019-04-24 23:37:21 +0800

    添加java8样例代码

---
+++ jdk8-master/src/main/java/com/shengsiyuan/jdk8/OptionalTest2.java
@@ -0,0 +1,37 @@
+package com.shengsiyuan.jdk8;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalTest2 {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("zhangsan");
+
+        Employee employee2 = new Employee();
+        employee2.setName("lisi");
+
+        Company company = new Company();
+        company.setName("company1");
+
+        List<Employee> employees = Arrays.asList(employee, employee2);
+//        company.setEmployees(employees);
+
+        List<Employee> list = company.getEmployees();
+
+        Optional<Company> optional = Optional.ofNullable(company);
+
+        System.out.println(optional.map(theCompany -> theCompany.getEmployees()).
+                orElse(Collections.emptyList()));
+
+
+    }
+
+    public void test(Optional optional) {
+
+    }
+}
