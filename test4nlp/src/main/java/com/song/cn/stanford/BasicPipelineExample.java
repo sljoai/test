6461081944485b1c4ed48849560fdc23b7b8aad9
Administrator <sljoai@outlook.com>
2019-05-31 18:03:14 +0800

    添加nlp

---
+++ test4nlp/src/main/java/com/song/cn/stanford/BasicPipelineExample.java
@@ -0,0 +1,27 @@
+package com.song.cn.stanford;
+
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.Properties;
+
+public class BasicPipelineExample {
+    public static void main(String[] args) {
+
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "...";
+
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+    }
+
+}
