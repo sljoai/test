dbe69ba10fba00719714b7a7a7302d0c9746b84c
Administrator <sljoai@outlook.com>
2019-06-04 12:19:58 +0800

    添加KafkaAdminClient，动态增加分区

--- test4kafka/src/main/java/com/song/cn/kafka/controller/AdminClientTest.java
+++ test4kafka/src/main/java/com/song/cn/kafka/controller/AdminClientTest.java
@@ -1,31 +1,17 @@
 package com.song.cn.kafka.controller;
 
-import kafka.admin.AdminClient;
-import kafka.coordinator.group.GroupOverview;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.admin.AdminClient;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.Properties;
 
 public class AdminClientTest {
 
     public static void main(String[] args) {
         String brokerUrl = "172.16.117.5:9092";
         String topic = "auto_title";
-        AdminClient client = AdminClient.createSimplePlaintext(brokerUrl);
-        List<GroupOverview> allGroups = scala.collection.JavaConversions.seqAsJavaList(client.listAllGroupsFlattened().toSeq());
-        Set<String> groups = new HashSet<>();
-        for (GroupOverview overview : allGroups) {
-            String groupID = overview.groupId();
-            Map<TopicPartition, Object> offsets = scala.collection.JavaConversions.mapAsJavaMap(client.listGroupOffsets(groupID));
-            Set<TopicPartition> partitions = offsets.keySet();
-            for (TopicPartition tp : partitions) {
-                if (tp.topic().equals(topic)) {
-                    groups.add(groupID);
-                }
-            }
-        }
+        Properties properties = new Properties();
+        AdminClient client = AdminClient.create(properties);
+
+
     }
 }
