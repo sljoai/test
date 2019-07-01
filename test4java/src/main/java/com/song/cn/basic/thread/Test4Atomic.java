fe0f602aa2510b39f1869da899c12797dee9abd3
songlaijiang <99026795@qq.com>
2019-07-02 07:37:37 +0800

    添加AtomicInteger与Queue的使用

---
+++ test4java/src/main/java/com/song/cn/basic/thread/Test4Atomic.java
@@ -0,0 +1,7 @@
+package com.song.cn.basic.thread;
+
+public class Test4Atomic {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test4java/src/main/java/com/song/cn/basic/thread/Test4Queue.java
@@ -0,0 +1,7 @@
+package com.song.cn.basic.thread;
+
+public class Test4Queue {
+    public static void main(String[] args) {
+
+    }
+}
